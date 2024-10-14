adf36b416a83326f5acae875d23bf0a384064554
Akshit Gupta <akshu.24gupta@gmail.com>
2024-10-14 20:31:26 +0530

    mobile bottom fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { EditorProvider } from "@/contexts/EditorContext";
 export default function Home() {
   return (
     <EditorProvider>
-      <div className="flex flex-col max-h-screen h-screen">
+      <div className="flex flex-col max-h-screen h-screen pb-20 lg:pb-0">
         <Navbar />
         <div className="flex-1">
           <Board />
